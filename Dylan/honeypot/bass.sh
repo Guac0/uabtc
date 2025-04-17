73c41496a96650bdffeeadb34ed1fbefa5872206
Dylan Weber <dylanweber06@gmail.com>
2025-04-17 18:09:42 -0400

    Added flag detection

--- Dylan/honeypot/bass.sh
+++ Dylan/honeypot/bass.sh
@@ -4,6 +4,9 @@ while true; do
   read -ra args -p "$(whoami)@$(hostname):$(pwd)\$ "
   if [[ "${args[0]}" == "ls" ]]; then
     for ((i = 1; i < ${#args[@]}; i++)); do
+      if [[ "${args[i]}" == -* ]]; then
+        continue
+      fi
       echo "ls: cannot access '${args[i]}': No such file or directory"
     done
   fi
