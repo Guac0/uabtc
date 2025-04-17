c17d5d0897a76d1b1574d2b09555344729a8ab02
Dylan Weber <dylanweber06@gmail.com>
2025-04-17 18:35:28 -0400

    Added exit trap

--- Dylan/honeypot/bass.sh
+++ Dylan/honeypot/bass.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-while true; do
-  read -ra args -p "$(whoami)@$(hostname):$(pwd)\$ "
-  echo "${args[@]}" >> bass.log
+trap 'printf "SIGINT caught! Use 'exit' to leave.\n$(whoami)@$(hostname):$(pwd)\$ "' SIGINT
+
+ls() {
   if [[ "${args[0]}" == "ls" ]]; then
     for ((i = 1; i < ${#args[@]}; i++)); do
       if [[ "${args[i]}" == -* ]]; then
@@ -11,4 +11,17 @@ while true; do
       echo "ls: cannot access '${args[i]}': No such file or directory"
     done
   fi
+}
+
+exit_fake() {
+  if [[ "${args[0]}" == "exit" ]]; then
+    echo "nuh uh"
+  fi
+}
+
+while true; do
+  read -ra args -p "$(whoami)@$(hostname):$(pwd)\$ "
+  echo "${args[@]}" >>bass.log
+  ls
+  exit_fake
 done
